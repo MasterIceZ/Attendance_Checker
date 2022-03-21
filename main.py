4c88581c1605eb8c9aedafcda7773e45512cf92d
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2022-03-21 09:14:56 +0700

    burn all things in main into functions

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 import pandas as pd
 
 
-debug_mode = False
+# Global Variables
+name_map = dict()
+name_idx = dict()
+data = dict()
+all_names = []
+
+def read_data(fileName:str):
+    attend = pd.read_excel(fileName, sheet_name="Attendance")
+
+    names = list()
+
+    for name in attend['Attendee']:
+        if name not in name_map.keys():
+            continue
+        real_name = name_map[name]
+        names.append(real_name)
+
+    # Fill Found
+    for i in range(len(name_idx)):
+        if name_idx[i] in names:
+            data[i][fileName] = 1
+    # Fill Not Found
+    for i in range(len(all_names)):
+        current_name = all_names[i]
+        if current_name in names:
+            continue
+        data[i][fileName] = 0
+    return 
+
 
 def main():
+    
+    # Initial State
     all_students = pd.read_excel('students.xlsx', sheet_name="Students")
-
-    # Must be update in 1.2
-    # print(all_students.head)
-    # date = input('date to day : ')
-    # interval = input('[M]orning/[A]fternoon/[E]vening : ')
-    # table_name = date + " " + interval
-    # print(table_name)
-    
-    data_set = ""
-    if debug_mode:
-        data_set = "testset.xlsx"
-    else:
-        data_set = input("Enter Data File's name : ")
-
-    attend = pd.read_excel(dataset, sheet_name="Attendance")
-    # print(attend.head)
-
-    students = dict()
-    name_map = dict()
-
-    for name in all_students['Name']:
-        students[name] = False
-    
-    all_names = []
 
     for i in range(len(all_students['Name'])):
         current_user = all_students['Username'][i]
@@ -36,36 +42,17 @@ def main():
         name_map[current_user] = current_name
         all_names.append(current_name)
 
-    # print(name_map)
-    # print(students.keys())
+    for i in range(len(all_names)):
+        data[i] = dict()
+        data[i]["Name"] = all_names[i]
+        name_idx[i] = all_names[i]
 
-    names = []
-    at = []
+    # Solving Session
+    read_data("20_3_Afternoon.xlsx")
+    read_data("20_3_Evening.xlsx")
 
-    for name in attend['Attendee']:
-#        print("Name : " + name)
-        if name not in name_map.keys():
-            continue
-        real_name = name_map[name]
-        print(real_name)
-        students[real_name] = True
-        at.append(True)
-        names.append(real_name)
-
-    print(students)
-        
-    it = 0
-
-    while(len(names) != len(students)):
-        while(all_names[it] in names):
-            it += 1
-        names.append(all_names[it])
-        at.append(False)
-
-    data = {}
-    for i in range(len(students)) :
-        data[i] = {"Name": names[i], "Attend": at[i]}
- 
+    # Write Output file
+    # Don't Touch
     writer = pd.ExcelWriter("Output.xlsx", engine="xlsxwriter")
     df = pd.DataFrame(data)
     df = df.transpose()
@@ -75,6 +62,8 @@ def main():
     worksheet = writer.sheets["Fuck Off"]
 
     writer.save()
+    
+    # Debug if cannot finish
     print("Finished");
 
 
